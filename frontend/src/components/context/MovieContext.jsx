7f4b7573c580bc53eca4fddb5a13d0bb08f537c4
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-06-06 12:38:47 +0530

    Update MovieContext.jsx

--- frontend/src/components/context/MovieContext.jsx
+++ frontend/src/components/context/MovieContext.jsx
@@ -370,9 +370,12 @@ export const MovieProvider = ({ children }) => {
     if (activeServer === "server1") {
       // Server 1 (vidlink.pro)
       if (mediaType === "movie") {
-        return `https://player.vidsrc.co/embed/movie/${id}`;
+        // return `https://player.vidsrc.c/embed/movie/${id}`;
+        return `https://player.embed-api.stream/?${id}&type=movie`;
       } else if (mediaType === "tv" && season && episode) {
-        return `https://player.vidsrc.co/embed/tv/${id}/${season}/${episode}`;
+        // return `https://player.vidsrc.co/embed/tv/${id}/${season}/${episode}`;
+        // https://player.embed-api.stream/?id={tmdbId}&s={season}&e={episode}
+        return `https://player.vidsrc.co/?${id}&s=${season}&e=${episode}`;
       }
     } else {
       // Server 2 (vidsrc.icu)
