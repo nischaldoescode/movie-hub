3b51aa764c3397892845f8c9373c840a6e46b398
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-07-04 14:53:56 +0530

    Update MoviePage.jsx

--- frontend/src/components/pages/MoviePage.jsx
+++ frontend/src/components/pages/MoviePage.jsx
@@ -54,10 +54,10 @@ const MoviePage = () => {
   const scrollRestoreRef = useRef(null);
 
   const popupPatterns =
-    /pop|click|redirect|offer|win|prize|free|bonus|ad|banner|track|survey|smart|clk|advert|campaign|campaign|IOarzRhPlPOverlay|modal|selectextShadowHos|shadow|znid|donto|popcash|display|osumpdfciiptn|muthwhcjuwela|qtdfxjlbnojnc|pl-d8e112f909ccf659971eeb2e95e5128c__wrap|pl-d8e112f909ccf659971eeb2e95e5128c__content|pl-d8e112f909ccf659971eeb2e95e5128c__btn-block|pl-d8e112f909ccf659971eeb2e95e5128c__bt|pl-d8e112f909ccf659971eeb2e95e5128c__link pl-d8e112f909ccf659971eeb2e95e5128c__finlink|pl-d8e112f909ccf659971eeb2e95e5128c__content-block|pl-d8e112f909ccf659971eeb2e95e5128c__desc-wrap|pl-d8e112f909ccf659971eeb2e95e5128c__desc/i;
+    /pop|click|redirect|offer|win|prize|free|bonus|ad|banner|track|survey|smart|clk|advert|campaign|campaign|IOarzRhPlPOverlay|modal|selectextShadowHos|shadow|znid|donto|popcash|display|osumpdfciiptn|muthwhcjuwela|qtdfxjlbnojnc|pl-d8e112f909ccf659971eeb2e95e5128c__wrap|pl-d8e112f909ccf659971eeb2e95e5128c__content|pl-d8e112f909ccf659971eeb2e95e5128c__btn-block|pl-d8e112f909ccf659971eeb2e95e5128c__bt|pl-d8e112f909ccf659971eeb2e95e5128c__link pl-d8e112f909ccf659971eeb2e95e5128c__finlink|pl-d8e112f909ccf659971eeb2e95e5128c__content-block|pl-d8e112f909ccf659971eeb2e95e5128c__desc-wrap|pl-d8e112f909ccf659971eeb2e95e5128c__desc|bbmfcst/i;
   const blockedDomainsPattern =
     /doubleclick|adservice|adnxs|adsystem|adsrvr|taboola|outbrain|revcontent|zedo|adroll|rubiconproject|openx|criteo|pubmatic|smartadserver|adtechus|quantserve|mediamath|turn|intellipopup|popcash|custom|effectivemeasure|tjwsg|osumpdfciiptn|\.online|muthwhcjuwela|\.store|qtdfxjlbnojnc|raggedstriking|usrpubtrk|brightadnetwor|storageimagedisplay|recordedthereby|nannyirrationalacquainted/i;
-
+  
   useEffect(() => {
     if (!streamingUrl || !playerProtected) return;
 
