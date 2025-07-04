d8ae923909a82b3ac536000cc9bc1751576c4474
Nischaldoescode <116507248+nischaldoescode@users.noreply.github.com>
2025-07-05 02:30:05 +0530

    Update Horizontal.jsx

--- frontend/src/components/ui/Horizontal.jsx
+++ frontend/src/components/ui/Horizontal.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"
 
-const HorizontalAd = () => {
+const SideAd = () => {
   useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error("AdSense error", e);
+    const pushAd = () => {
+      try {
+        const adsbygoogle = window.adsbygoogle
+        console.log({ adsbygoogle })
+        adsbygoogle.push({})
+      } catch (e) {
+        console.error(e)
+      }
     }
-  }, []);
 
+    let interval = setInterval(() => {
+      // Check if Adsense script is loaded every 300ms
+      if (window.adsbygoogle) {
+        pushAd()
+        // clear the interval once the ad is pushed so that function isn't called indefinitely
+        clearInterval(interval)
+      }
+    }, 300)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   return (
     <ins className="adsbygoogle"
          style={{ display: "block" }}
          data-ad-client="ca-pub-8779876482236769"
          data-ad-slot="9980651939"
          data-ad-format="auto"
-         data-full-width-responsive="true"></ins>
-  );
-};
+         data-full-width-responsive="true">
+    </ins>
+  )
+}
 
-export default HorizontalAd;
+export default SideAd
